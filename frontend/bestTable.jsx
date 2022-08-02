e2fd7ba79d87e1450b47f98a5f5b44daa3995900
your name here <roelliga@yahoo.com>
2022-08-02 15:50:18 -0700

    webpack

---
+++ frontend/bestTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>Best Table</h1>, rootEl);
+});
