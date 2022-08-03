9b30132fb2fb7d57d89ada93d98bc49bbe605556
your name here <roelliga@yahoo.com>
2022-08-03 16:00:37 -0700

    containers

---
+++ frontend/components/session/login_form_container.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { login, clearErrors } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors.session,
+    formType: 'Log in',
+    navLink: <Link to="/signup">Sign up</Link>,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (user) => dispatch(login(user)),
+    loginDemoUser: () => dispatch(login({username: "guestas3", password: "password13", email: 'emailA@'})),
+    clearErrors: () => dispatch(clearErrors())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
---
+++ frontend/components/session/signup_form_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { signup, clearErrors } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = state => {
+  return {
+    errors: state.errors.session,
+    formType: 'Sign up',
+    navLink: <Link to="/login">Log in</Link>,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (user) => dispatch(signup(user)),
+    clearErrors: () => dispatch(clearErrors())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
