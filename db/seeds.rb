5ebe6d0006942138651011e080e71f1e5d46e2b8
your name here <roelliga@yahoo.com>
2022-08-03 14:17:39 -0700

    sessions controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+User.delete_all
+
+u0 = User.create!({username: "andrew", password: "andy12", email: 'andy@andy.com'})
