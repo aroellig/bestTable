90304219b8bbd3efe1210dda17752afc0661b838
your name here <roelliga@yahoo.com>
2022-08-10 15:44:09 -0700

    restaurants reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
-
+import restaurantsReducer from './restaurants_reducer'
 
 const entitiesReducer = combineReducers({
 
-  users: usersReducer
+  users: usersReducer,
+  restaurants: restaurantsReducer
 });
 
 export default entitiesReducer
