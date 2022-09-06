07b10a336987a461dcd6773d1913e22504a700e9
your name here <roelliga@yahoo.com>
2022-09-06 14:34:51 -0700

    reservation model

---
+++ app/models/reservation.rb
@@ -0,0 +1,11 @@
+class Reservation < ApplicationRecord
+    validate :name, :number
+
+    belongs_to :user,
+    foreign_key: :user_id,
+    class_name: 'User'
+
+    belongs_to :restaurant,
+    foreign_key: :restaurant_id,
+    class_name: 'Restaurant'
+end
